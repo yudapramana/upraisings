f8710f910387e50c9c3967c30c465cd5deeafd7e
pramana yuda <pramanayuda772@gmail.com>
2025-07-29 23:10:42 +0700

    push

--- app/Http/Controllers/Partner/ProfileController.php
+++ app/Http/Controllers/Partner/ProfileController.php
@@ -33,7 +33,7 @@ class ProfileController extends Controller
         // Validasi input
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
-            'mobile_phone' => 'nullable|string|max:15',
+            'email' => 'nullable|email',
             'city_register' => 'nullable|string|max:100',
             'bank_name' => 'nullable|string|max:100',
             'account_number' => 'nullable|string|max:50',
@@ -47,7 +47,7 @@ class ProfileController extends Controller
         // Update data user
         $user->update([
             'name' => $request->name,
-            'mobile_phone' => $request->mobile_phone,
+            'email' => $request->email,
             'city_register' => $request->city_register,
             'bank_name' => $request->bank_name,
             'account_number' => $request->account_number,
